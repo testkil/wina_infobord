37b24f3b38c2d10ce41fe7b6cd96c72e6e43b292
Alex Camacho <mctestkil@gmail.com>
2024-10-06 17:49:00 +0200

    .

--- src/config.js
+++ src/config.js
@@ -1,8 +1,8 @@
-// export const DATA_ENDPOINT =
-//   "https://shop.wina-gent.be/controlepaneel/infoboard/json";
-
 export const DATA_ENDPOINT =
-  "http://localhost:8000/controlepaneel/infoboard/json";
+  "https://shop.wina-gent.be/controlepaneel/infoboard/json";
+
+// export const DATA_ENDPOINT =
+//   "http://localhost:8000/controlepaneel/infoboard/json";
 
 export const DATA_REFRESH_INTERVAL_MINUTES = 5;
 
